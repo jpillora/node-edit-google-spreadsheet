df7322b299d1739cae627e97336620c67b11e6f2
Jaime Pillora <dev@jpillora.com>
2017-09-24 20:59:43 +1000

    remove logs

--- example/read-basic-async-await.js
+++ example/read-basic-async-await.js
@@ -3,14 +3,12 @@ const creds = require("./cred-loader");
 
 (async () => {
   try {
-    console.log("load....");
     let spreadsheet = await Spreadsheet.load({
       debug: true,
       oauth2: creds,
       spreadsheetName: "node-spreadsheet-example",
       worksheetName: "Sheet1"
     });
-    console.log("loaded");
     //receive all cells
     let [rows, info] = await spreadsheet.receive({getValues: false});
     console.log("Found rows:", rows);
